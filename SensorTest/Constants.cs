70eb2a68ae4e7e72466835655645dc981cbf566b
Ankan Mookherjee <tupaimook@gmail.com>
2014-11-04 16:49:08 -0500

    Update Constants.cs
    Added extra comments

--- SensorTest/Constants.cs
+++ SensorTest/Constants.cs
@@ -4,14 +4,13 @@ namespace SenTest
 {
     public static class Constants
     {
-        // TODO: Configure the MobileServiceClient to communicate with your mobile service by
-        // uncommenting the following code and replacing AppUrl & AppKey with values from  
-        // your mobile service, which are obtained from the Windows Azure Management Portal.
-        // Do this after you add a reference to the Mobile Services client to your project.
-
+        /* Configure the MobileServiceClient to communicate with your mobile service by
+         uncommenting the following code and replacing AppUrl & AppKey with values from  
+        your mobile service, which are obtained from the Windows Azure Management Portal.
+	*/
         // Azure app specific URL and key
-		public const string ApplicationURL = @"https://sentest.azure-mobile.net/";
-		public const string ApplicationKey = @"jTPtOwRzDBqIFUoXvjRkfNqhIUqbcl85";
+	public const string ApplicationURL = @"https://sentest.azure-mobile.net/";
+	public const string ApplicationKey = @"jTPtOwRzDBqIFUoXvjRkfNqhIUqbcl85";
     }
 }
 
