bb1a5a19222744637435e96fce1d6921e17a01bf
Ankan Mookherjee <tupaimook@gmail.com>
2014-11-04 16:30:12 -0500

    Version 3.0
    This one is a client which would record the data to the back end

---
+++ SensorTest/Constants.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SenTest
+{
+    public static class Constants
+    {
+        // TODO: Configure the MobileServiceClient to communicate with your mobile service by
+        // uncommenting the following code and replacing AppUrl & AppKey with values from  
+        // your mobile service, which are obtained from the Windows Azure Management Portal.
+        // Do this after you add a reference to the Mobile Services client to your project.
+
+        // Azure app specific URL and key
+		public const string ApplicationURL = @"https://sentest.azure-mobile.net/";
+		public const string ApplicationKey = @"jTPtOwRzDBqIFUoXvjRkfNqhIUqbcl85";
+    }
+}
+
